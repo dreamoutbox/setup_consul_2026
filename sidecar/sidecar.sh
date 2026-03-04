8ce067c5b8d8ecc8b45518be8f61c31e758348f3
dreamoutbox <262966751+dreamoutbox@users.noreply.github.com>
2026-03-04 17:20:32 +0700

    use service name instead of ip

--- sidecar/sidecar.sh
+++ sidecar/sidecar.sh
@@ -9,8 +9,8 @@ while true; do
 
     CONSUL_ADDR='';
     while [ -z "$CONSUL_ADDR" ]; do
-        for addr in 10.0.0.2:8500 10.0.0.3:8500 10.0.0.4:8500 10.0.0.5:8500 10.0.0.6:8500; do
-            if wget -T 3 -qO- http://$addr/v1/agent/services 2>/dev/null | grep -q "${SIDECAR_NAME}-sidecar-proxy"; then
+        for addr in consul1:8500 consul2:8500 consul3:8500 consul4:8500 consul5:8500; do
+            if wget -T 2 -qO- http://$addr/v1/agent/services 2>/dev/null | grep -q "${SIDECAR_NAME}-sidecar-proxy"; then
             CONSUL_ADDR=$addr;
             break;
             fi;
@@ -18,7 +18,7 @@ while true; do
 
         [ -z "$CONSUL_ADDR" ] && sleep 1;
     done;
-    
+
     echo "========================================================="
     echo "[$SIDECAR_NAME] Using Consul at $CONSUL_ADDR";
     echo "========================================================="
@@ -28,16 +28,16 @@ while true; do
     PROXY_PID=$!;
 
     while kill -0 $PROXY_PID 2>/dev/null; do
-    if ! wget -T 5 -qO- http://$CONSUL_ADDR/v1/status/leader 2>/dev/null | grep -q '.' ; then
-        echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-        echo "[$SIDECAR_NAME] Consul at $CONSUL_ADDR unreachable, killing proxy...";
-        echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        if ! wget -T 1 -qO- http://$CONSUL_ADDR/v1/status/leader 2>/dev/null | grep -q '.' ; then
+            echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            echo "[$SIDECAR_NAME] Consul at $CONSUL_ADDR unreachable, killing proxy...";
+            echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
-        kill $PROXY_PID 2>/dev/null;
-        wait $PROXY_PID 2>/dev/null;
-        break;
-    fi;
-    sleep 3;
+            kill $PROXY_PID 2>/dev/null;
+            wait $PROXY_PID 2>/dev/null;
+            break;
+        fi;
+        sleep 3;
     done;
 
     echo "[$SIDECAR_NAME] proxy exited, restarting...";
