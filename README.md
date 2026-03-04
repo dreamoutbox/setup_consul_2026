cfeaebb3dac4426ca5a57149cb79d9dea28a6a53
dreamoutbox <262966751+dreamoutbox@users.noreply.github.com>
2026-03-04 17:01:02 +0700

    working HA consul

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
 # setup_consul_2026
+
 trying service mesh consul
 
 ## Quickstart
 
 Run it!
+
 ```bash
 docker compose down && docker compose up -d --build
 
 ```
 
 See sidecar logs
+
 ```bash
 docker logs service-a-sidecar
 docker logs service-b-sidecar
 ```
 
 See services logs
+
 ```bash
 docker logs service-a
 docker logs service-b
 ```
 
 and you should see output like this
+
 ```
 Start service-a
 error request to service-b Get "http://localhost:9191": dial tcp [::1]:9191: connect: connection refused
@@ -33,4 +38,25 @@ SUCCESS!
 service-b called: Hello from service B
 
 service-b listening on 127.0.0.1:8080
+```
+
+## HA Consul
+
+Stop `consul1` and `consul2` to simulate failure
+
+```bash
+docker stop consul1 consul2
+```
+
+Restart `service-a` `service-a-sidecar` `service-b-sidecar` to make them connect to `consul3` `consul4` `consul5` and reload stale certificates
+
+```bash
+docker restart service-a service-a-sidecar service-b-sidecar
+```
+
+See service-a still able to call service-b
+
+```bash
+docker logs service-a-sidecar
+docker logs service-a
 ```
---
+++ sidecar/sidecar.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+export SIDECAR_NAME=$1
+
+while true; do
+    echo "========================================================="
+    echo "[$SIDECAR_NAME] discovering healthy Consul server...";
+    echo "========================================================="
+
+    CONSUL_ADDR='';
+    while [ -z "$CONSUL_ADDR" ]; do
+        for addr in 10.0.0.2:8500 10.0.0.3:8500 10.0.0.4:8500 10.0.0.5:8500 10.0.0.6:8500; do
+            if wget -T 3 -qO- http://$addr/v1/agent/services 2>/dev/null | grep -q "${SIDECAR_NAME}-sidecar-proxy"; then
+            CONSUL_ADDR=$addr;
+            break;
+            fi;
+        done;
+
+        [ -z "$CONSUL_ADDR" ] && sleep 1;
+    done;
+    
+    echo "========================================================="
+    echo "[$SIDECAR_NAME] Using Consul at $CONSUL_ADDR";
+    echo "========================================================="
+    export CONSUL_HTTP_ADDR=$CONSUL_ADDR;
+
+    consul connect proxy -sidecar-for $SIDECAR_NAME -log-level warn &
+    PROXY_PID=$!;
+
+    while kill -0 $PROXY_PID 2>/dev/null; do
+    if ! wget -T 5 -qO- http://$CONSUL_ADDR/v1/status/leader 2>/dev/null | grep -q '.' ; then
+        echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        echo "[$SIDECAR_NAME] Consul at $CONSUL_ADDR unreachable, killing proxy...";
+        echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+        kill $PROXY_PID 2>/dev/null;
+        wait $PROXY_PID 2>/dev/null;
+        break;
+    fi;
+    sleep 3;
+    done;
+
+    echo "[$SIDECAR_NAME] proxy exited, restarting...";
+    sleep 1;
+done
